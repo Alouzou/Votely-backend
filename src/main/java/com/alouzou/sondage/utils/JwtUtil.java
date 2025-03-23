4e9789f81292ea71e22f49e7276f25a7b10b4a76
Amine <amineouzou@gmail.com>
2025-03-22 21:25:04 -0400

    utils

--- src/main/java/com/alouzou/sondage/utils/JwtUtil.java
+++ src/main/java/com/alouzou/sondage/utils/JwtUtil.java
@@ -1,2 +1,16 @@
-package com.alouzou.sondage.utils;public class JwtUtil {
+package com.alouzou.sondage.utils;
+
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.security.Keys;
+import org.springframework.stereotype.Component;
+
+import java.security.Key;
+
+@Component
+public class JwtUtil {
+    private static final String SECRET_KEY = "CeciEstUneCleSecreteSuperLonguePourJWT2025!!";
+    private static final long EXPIRATION_TIME = 86400000; // 1 jour
+
+    private final Key key = Keys.hmacShaKeyFor(SECRET_KEY.getBytes());
+
 }
