3bd3a730cede8db22393e87a2878738b346c95dd
Amine <amineouzou@gmail.com>
2025-04-15 23:17:09 -0400

    ajouté l

--- src/main/java/com/alouzou/sondage/controllers/CategoryController.java
+++ src/main/java/com/alouzou/sondage/controllers/CategoryController.java
@@ -2,22 +2,28 @@ package com.alouzou.sondage.controllers;
 
 import com.alouzou.sondage.entities.Category;
 import com.alouzou.sondage.services.CategoryService;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
 @RestController
+@Slf4j
 @RequestMapping("/api/categories")
 public class CategoryController {
 
 
     @Autowired
     private CategoryService categoryService;
+
+
+    @PostMapping("/create")
+    public ResponseEntity<Category> createCategory(@RequestBody Category category){
+        Category cat = categoryService.createCategory(category.getName(), category.isActive());
+        return ResponseEntity.ok(cat);
+    }
 
     @GetMapping("/all")
     public ResponseEntity<List<Category>> findAll(){
@@ -31,7 +37,4 @@ public class CategoryController {
                 .map(ResponseEntity::ok)
                 .orElse(ResponseEntity.notFound().build());
     }
-
-
-
 }
