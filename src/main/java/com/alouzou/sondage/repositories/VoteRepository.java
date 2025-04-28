095930c0cf2e7dd8a7c91e0459c20fd364f30d84
Amine <amineouzou@gmail.com>
2025-04-27 20:45:30 -0400

    feat: un utilisateur peut voter seuleuement une seul efois à un sondage

--- src/main/java/com/alouzou/sondage/repositories/VoteRepository.java
+++ src/main/java/com/alouzou/sondage/repositories/VoteRepository.java
@@ -2,9 +2,17 @@ package com.alouzou.sondage.repositories;
 
 import com.alouzou.sondage.entities.Vote;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface VoteRepository extends JpaRepository<Vote, Long> {
     boolean existsByUserIdAndChoiceId(Long userId, Long choiceId);
+
+    @Query("SELECT CASE WHEN COUNT(v) > 0 THEN true ELSE false END " +
+            "FROM Vote v " +
+            "WHERE v.user.id = :userId " +
+            "AND v.choice.question.survey.id = :surveyId")
+    boolean existsByUserIdAndSurveyId(@Param("userId") Long userId, @Param("surveyId") Long surveyId);
 }
--- src/main/java/com/alouzou/sondage/services/Impl/VoteServiceImpl.java
+++ src/main/java/com/alouzou/sondage/services/Impl/VoteServiceImpl.java
@@ -2,9 +2,11 @@ package com.alouzou.sondage.services.Impl;
 
 import com.alouzou.sondage.dto.VoteDTO;
 import com.alouzou.sondage.entities.Choice;
+import com.alouzou.sondage.entities.Survey;
 import com.alouzou.sondage.entities.User;
 import com.alouzou.sondage.entities.Vote;
 import com.alouzou.sondage.exceptions.EntityNotFoundException;
+import com.alouzou.sondage.exceptions.ForbiddenActionException;
 import com.alouzou.sondage.repositories.ChoiceRepository;
 import com.alouzou.sondage.repositories.VoteRepository;
 import com.alouzou.sondage.repositories.UserRepository;
@@ -28,16 +30,30 @@ public class VoteServiceImpl implements VoteService {
         this.voteRepository = voteRepository;
     }
 
+    public boolean hasUserAlreadyVotedForSurvey(Long choiceId, Long userId){
+        Choice choice = choiceRepository.findById(choiceId)
+                .orElseThrow(() -> new EntityNotFoundException("Choix non trouvé"));
+
+        Long surveyId = choice.getQuestion().getSurvey().getId();
+
+        return voteRepository.existsByUserIdAndSurveyId(userId, surveyId);
+
+    }
+
     @Override
     public Vote vote(VoteDTO dto) {
         User userPrincipal = authService.getCurrentUser();
-        if (voteRepository.existsByUserIdAndChoiceId(userPrincipal.getId(), dto.getChoiceId())) {
-            throw new IllegalArgumentException("L'utilisateur a déjà voté pour ce choix.");
-        }
         User user = userRepository.findById(userPrincipal.getId())
                 .orElseThrow(() -> new EntityNotFoundException("Utilisateur introuvable"));
         Choice choice = choiceRepository.findById(dto.getChoiceId())
                 .orElseThrow(() -> new EntityNotFoundException("Choix introuvable"));
+
+        if (voteRepository.existsByUserIdAndChoiceId(userPrincipal.getId(), dto.getChoiceId())) {
+            throw new IllegalArgumentException("L'utilisateur a déjà voté pour ce choix.");
+        }
+        if(hasUserAlreadyVotedForSurvey(dto.getChoiceId(), user.getId())){
+            throw new ForbiddenActionException("Vous avez déjà voté pour ce sondage.");
+        }
 
         Vote vote = Vote.builder()
                 .user(user)
