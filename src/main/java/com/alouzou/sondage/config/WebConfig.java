c481e80a43007350c8bf95906f12271b5ef6d019
Amine <amineouzou@gmail.com>
2025-11-11 17:40:41 -0500

    update webconfig

--- src/main/java/com/alouzou/sondage/config/WebConfig.java
+++ src/main/java/com/alouzou/sondage/config/WebConfig.java
@@ -8,8 +8,7 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/api/**")
-                .allowedOrigins("http://localhost:4200")
-                .allowedOrigins("https://votely.alouzou.dev")
+                .allowedOrigins("http://localhost:4200", "https://votely.alouzou.dev")
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                 .allowedHeaders("*")
                 .exposedHeaders("Authorization")
