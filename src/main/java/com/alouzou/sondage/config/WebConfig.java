1f1a28cc3911c21d7e8f1ecba406a152142e3b17
Amine <amineouzou@gmail.com>
2025-11-11 16:53:57 -0500

    cors allows origin votely.alouzou.dev

--- src/main/java/com/alouzou/sondage/config/WebConfig.java
+++ src/main/java/com/alouzou/sondage/config/WebConfig.java
@@ -9,6 +9,7 @@ public class WebConfig implements WebMvcConfigurer {
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/api/**")
                 .allowedOrigins("http://localhost:4200")
+                .allowedOrigins("https://votely.alouzou.dev")
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                 .allowedHeaders("*")
                 .exposedHeaders("Authorization")
